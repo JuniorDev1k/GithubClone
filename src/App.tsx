3455b218ac89f3d07124e54a23220902401d1130
JuniorDev1k <ayoubdev1k@gmail.com>
2024-08-04 18:25:08 +0200

    feat(routing): added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import {
+  Homescreen,
+  LoginScreen,
+  SignupScreen,
+  CreatePost,
+  NotFound,
+  RepositoryScreen,
+} from "./pages";
 type Props = {};
 
 const App = (props: Props) => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Homescreen />} />
+        <Route path="/RepositoryScreen" element={<RepositoryScreen />} />
+        <Route path="/CreatePost" element={<CreatePost />} />
+        <Route path="/signup" element={<SignupScreen />} />
+        <Route path="/Login" element={<LoginScreen />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Homescreen from "./HomeScreen";
+import LoginScreen from "./LoginScreen";
+import RepositoryScreen from "./RepositoryScreen";
+import SignupScreen from "./SignupScreen";
+import NotFound from "./NotFound";
+import CreatePost from "./CreatePost";
+
+export {
+  Homescreen,
+  LoginScreen,
+  RepositoryScreen,
+  SignupScreen,
+  CreatePost,
+  NotFound,
+};
