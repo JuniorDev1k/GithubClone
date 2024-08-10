ac17b6cc3f9075a1dd2ec246a7e4759618cde12e
JuniorDev1k <ayoubdev1k@gmail.com>
2024-08-10 15:04:10 +0200

    feat(fetch)added functions Fetching data from Github public Api

--- src/services/githubApi.tsx
+++ src/services/githubApi.tsx
@@ -1,3 +1,51 @@
-// export default const GetUser = async (userName) => {
-//     const response = fetch('')
-// }
+// Gtihub Public Api endPoints:
+
+// Fetch User
+export const GetUser = async (username: string) => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+  } catch (error) {
+    console.log("error:", error);
+  }
+};
+
+// Search Users .
+export const SearchUser = async (searchedUser: string) => {
+  try {
+    const response = fetch(
+      `https://api.github.com/search/users?q=${searchedUser}`
+    );
+  } catch (error) {
+    console.log("error:", error);
+  }
+};
+
+// List  + search  repo for a Specefic User.
+export const RepositoryList = async (
+  username: string,
+  searchTerm: string = ""
+) => {
+  const response = await fetch(
+    `https://api.github.com/users/${username}/repos`
+  );
+  const repos = await response.json();
+
+  if (searchTerm) {
+    return repos.filter((repo: any) =>
+      repo.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+
+  return repos;
+};
+
+// Search Popular Repos.
+export const SearchRepos = async (repo: string) => {
+  try {
+    const response = await fetch(
+      `https://api.github.com/search/repositories?${repo}`
+    );
+  } catch (error) {
+    console.log("error:", error);
+  }
+};
